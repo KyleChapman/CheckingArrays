fa5ad9a22d8db72e579a4cbc5b5f2d14b94c73f7
Kyle Chapman <kyle.chapman@durhamcollege.ca>
2021-02-07 20:15:51 -0500

    Fixed up the (still crappy) documentation.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Written by: Kyle Chapman
+// with some coaching from NETD 2202-01 W2021.
+// Date: Feb 1, 2021
+// This is a proof-of-concept related to array indexes to (maybe?) clear up
+// some confusion about array indexes in C#.
+
+using System;
 
 namespace CheckingArrays
 {
@@ -18,6 +24,7 @@ namespace CheckingArrays
             integerArray[2, 0] = 10;
             integerArray[2, 1] = 12;
 
+            // Loop through the array and output all indexes.
             for(int rowCounter = 0; rowCounter < integerArray.GetLength(0); rowCounter++)
             {
                 for (int columnCounter = 0; columnCounter < integerArray.GetLength(1); columnCounter++)
@@ -26,11 +33,14 @@ namespace CheckingArrays
                 }
             }
 
+            // A few additional array properties we wanted to check.
             Console.WriteLine("Length of dimension 0: " + integerArray.GetLength(0));
             Console.WriteLine("Length of dimension 1: " + integerArray.GetLength(1));
             Console.WriteLine("Upper bound of dimension 0: " + integerArray.GetUpperBound(0));
             Console.WriteLine("Upper bound of dimension 1: " + integerArray.GetUpperBound(1));
 
+            // This next line is intended to make the program crash.
+            // If you wanted to solve it, you could use GetUpperBound() instead of GetLength().
             //Console.WriteLine("Hopefully this crashes: " + integerArray[0, integerArray.GetLength(1)]);
 
             Console.WriteLine("Length of the entire array: " + integerArray.Length);
